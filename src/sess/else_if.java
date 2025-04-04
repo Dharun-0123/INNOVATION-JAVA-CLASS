2c82be0f50fd2ccf407657bbd6565cbd3ce4edd4
Dharun-0123 <dharunkavin123@outlook.com>
2025-04-04 17:10:42 +0530

    session 2

--- src/sess/else_if.java
+++ src/sess/else_if.java
@@ -1,21 +1,22 @@
 package sess;
 
 public class else_if {
+	static int age;
 public static void main(String[] args) {
 	 
-	 int age = 25; // Example: Customer's age
+	
    
      double ticketPrice;
 
      if (age < 12) {
-         ticketPrice = 5.0; // Child price
+         ticketPrice = 50; // Child price
      } else if (age >= 12 && age <= 18) {
-         ticketPrice = 7.0; // Teen price
+         ticketPrice = 70; // Teen price
      } else if (age > 60) {
-         ticketPrice = 6.0; // Senior citizen discount
+         ticketPrice = 60; // Senior citizen discount
      } else {
-         ticketPrice = 10.0; // Standard price
+         ticketPrice = 100; // Standard price
      }
-     System.out.println("Your ticket price is: $" + ticketPrice);
+     System.out.println("Your ticket price is: Rs" + ticketPrice);
 }
 }
